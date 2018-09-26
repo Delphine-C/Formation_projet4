5f3fd374005b6cd5a3fd5f82a697508004748033
Delphine-C <corneil.delphine@gmail.com>
2018-09-26 12:03:34 +0200

    New bundle called LouvresTicketingBundle

---
+++ src/Louvres/TicketingBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Louvres\TicketingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@LouvresTicketing/Default/index.html.twig');
+    }
+}
---
+++ src/Louvres/TicketingBundle/LouvresTicketingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Louvres\TicketingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LouvresTicketingBundle extends Bundle
+{
+}
