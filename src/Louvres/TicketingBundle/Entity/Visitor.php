871bef8b0fb9d705cfc079c214014627a2e394fb
Delphine-C <corneil.delphine@gmail.com>
2018-09-27 15:44:35 +0200

    New entity visitor, create DB

---
+++ src/Louvres/TicketingBundle/Entity/Visitor.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Louvres\TicketingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Visitor
+ *
+ * @ORM\Table(name="visitor")
+ * @ORM\Entity(repositoryClass="Louvres\TicketingBundle\Repository\VisitorRepository")
+ */
+class Visitor
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthdate", type="date")
+     */
+    private $birthdate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="reduction", type="boolean")
+     */
+    private $reduction;
+
+    public function __construct()
+    {
+        $this->reduction = 0;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Visitor
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return Visitor
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Visitor
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set birthdate
+     *
+     * @param \DateTime $birthdate
+     *
+     * @return Visitor
+     */
+    public function setBirthdate($birthdate)
+    {
+        $this->birthdate = $birthdate;
+    }
+
+    /**
+     * Get birthdate
+     *
+     * @return \DateTime
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    /**
+     * Set reduction
+     *
+     * @param boolean $reduction
+     *
+     * @return Visitor
+     */
+    public function setReduction($reduction)
+    {
+        $this->reduction = $reduction;
+    }
+
+    /**
+     * Get reduction
+     *
+     * @return bool
+     */
+    public function getReduction()
+    {
+        return $this->reduction;
+    }
+}
+
