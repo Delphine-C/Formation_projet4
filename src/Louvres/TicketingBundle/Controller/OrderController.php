226af4aaa42fd5428fbf9c713d89c3a7c1fb3e07
Delphine-C <corneil.delphine@gmail.com>
2018-10-14 11:37:17 +0200

    Delete PaymentController, action in OrderConroller

--- src/Louvres/TicketingBundle/Controller/OrderController.php
+++ src/Louvres/TicketingBundle/Controller/OrderController.php
@@ -16,6 +16,33 @@ class OrderController extends Controller
 {
     public function indexAction(Request $request)
     {
+        if ($request->isMethod('POST')) {
+            $apiSecret = $this->container->getParameter('API_secret');
+            \Stripe\Stripe::setApiKey($apiSecret);
+
+            $prix = $request->getSession()->get('prix') * 100;
+
+            try {
+                \Stripe\Charge::create(array(
+                    "amount" => $prix,
+                    "currency" => "eur",
+                    "source" => $request->request->get('stripeToken'),
+                    "description" => sprintf("Paiement de %s",$request->getSession()->get('resa')->getName()),
+                ));
+            } catch(\Stripe\Error\Card $e) {
+                $this->addFlash('error',"Le paiement de votre commande a rencontré un problème. Veuillez recommencer l'opération.");
+                return $this->redirectToRoute('louvres_ticketing_order');
+            }
+
+            $booking = $request->getSession()->get('resa');
+            $booking->setPrice($prix);
+            $this->getDoctrine()->getManager()->persist($booking);
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('notice',"Votre commande a été passée avec succès. Vos billets vous ont été envoyés par mail.");
+            return $this->redirectToRoute('louvres_ticketing_booking');
+        }
+
         $priceC = new PriceCalculator();
 
         $prix = $priceC->calculPrix($request);
