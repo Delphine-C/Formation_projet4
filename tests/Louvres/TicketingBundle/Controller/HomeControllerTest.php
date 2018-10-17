7c5cb8057ea0169b22671a73bd32aa26d0754049
Delphine-C <corneil.delphine@gmail.com>
2018-10-17 11:08:42 +0200

    First functional test: homepage response '200'

---
+++ tests/Louvres/TicketingBundle/Controller/HomeControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Delphine_Corneil
+ * Date: 17/10/2018
+ * Time: 10:36
+ */
+
+namespace Tests\Louvres\TicketingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testHomepageIsUp()
+    {
+        $client = static::createClient();
+        $client->request('GET','/');
+
+        $this->assertSame(200,$client->getResponse()->getStatusCode());
+    }
+}
