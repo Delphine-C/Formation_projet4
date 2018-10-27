1741d8193af4e0e1c4cbbe4a9a1fed1825309f8d
Delphine-C <corneil.delphine@gmail.com>
2018-10-27 11:10:36 +0200

    Add google reCaptcha on mail form

--- src/Louvres/TicketingBundle/Controller/ContactController.php
+++ src/Louvres/TicketingBundle/Controller/ContactController.php
@@ -23,27 +23,50 @@ class ContactController extends Controller
             ->handleRequest($request);
 
         if ($formContact->isSubmitted() && $formContact->isValid()) {
-            $monMail = $this->container->getParameter('mail_address');
+            // Test recaptcha
+            $secret = $this->container->getParameter('captcha_secret'); // votre clé privée
+            $response = $_POST['g-recaptcha-response'];// Paramètre renvoyé par le recaptcha
+            $remoteip = $_SERVER['REMOTE_ADDR'];// On récupère l'IP de l'utilisateur
+            $api_url = "https://www.google.com/recaptcha/api/siteverify?secret="
+                . $secret
+                . "&response=" . $response
+                . "&remoteip=" . $remoteip ;
 
-            $message = (new \Swift_Message("Un message via le Musée du Louvre"))
-                ->setFrom(['billetterie-louvre@mail.com' => 'Billetterie Louvre'])
-                ->setTo($monMail)
-                ->setBody(
-                    $this->renderView(
-                        '@LouvresTicketing/Mail/contact.html.twig', [
-                            'contact' => $contact
-                        ]
-                    ),
-                    'text/html'
-                );
-            $this->get('mailer')->send($message);
+            $decode = json_decode(file_get_contents($api_url), true);
 
-            $this->addFlash('notice', "Votre mail a bien été envoyé.");
-            return $this->redirectToRoute('louvres_ticketing_contact');
+            if ($decode['success'] == true) {
+                $monMail = $this->container->getParameter('mail_address');
+
+                $message = (new \Swift_Message("Un message via le Musée du Louvre"))
+                    ->setFrom(['billetterie-louvre@mail.com' => 'Billetterie Louvre'])
+                    ->setTo($monMail)
+                    ->setBody(
+                        $this->renderView(
+                            '@LouvresTicketing/Mail/contact.html.twig', [
+                                'contact' => $contact
+                            ]
+                        ),
+                        'text/html'
+                    );
+                $this->get('mailer')->send($message);
+
+                $this->addFlash('notice', "Votre mail a bien été envoyé.");
+                return $this->redirectToRoute('louvres_ticketing_contact');
+            }
+
+            else {
+                $this->addFlash('error', "Votre mail n'a pu être envoyé. Veuillez réessayer.");
+
+                return $this->render('@LouvresTicketing/Contact/contact.html.twig', [
+                    'form' => $formContact->createView(),
+                    'apiPublic' => $this->container->getParameter('captcha_public')
+                ]);
+            }
         }
 
         return $this->render('@LouvresTicketing/Contact/contact.html.twig', [
-            'form' => $formContact->createView()
+            'form' => $formContact->createView(),
+            'apiPublic' => $this->container->getParameter('captcha_public')
         ]);
     }
 }
